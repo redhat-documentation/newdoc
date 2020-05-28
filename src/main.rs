335f86bb074b0d052a5210e19b0942041c2accb0
Marek Suchánek <marek.suchanek@protonmail.com>
2020-05-28 19:27:29 +0200

    Add placeholder command-line options to match the Python newdoc

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,46 @@
 extern crate clap;
-use clap::App;
+use clap::{Arg, App};
 
 fn main() {
-    println!("Hello, world!");
-
-    App::new("newdoc")
+    // Define command-line options
+    let matches = App::new("newdoc")
         .version("v2.0.0")
+        .author("Marek Suchánek")
+        .about("Generate an AsciiDoc file using a modular template")
+        .arg(Arg::with_name("assembly")
+             .short("a")
+             .long("assembly")
+             .takes_value(true)
+             .value_name("title")
+             .multiple(true)
+             .help("Create an assembly file"))
+        .arg(Arg::with_name("concept")
+             .short("c")
+             .long("concept")
+             .takes_value(true)
+             .value_name("title")
+             .multiple(true)
+             .help("Create a concept module"))
+        .arg(Arg::with_name("procedure")
+             .short("p")
+             .long("procedure")
+             .takes_value(true)
+             .value_name("title")
+             .multiple(true)
+             .help("Create a procedure module"))
+        .arg(Arg::with_name("reference")
+             .short("r")
+             .long("reference")
+             .takes_value(true)
+             .value_name("title")
+             .multiple(true)
+             .help("Create a reference module"))
+        .arg(Arg::with_name("no-comments")
+             .short("C")
+             .long("no-comments")
+             .help("Generate the file without any comments"))
         .get_matches();
+
+    // List the passed command-line options, just for debugging
+    println!("{:#?}", matches);
 }
