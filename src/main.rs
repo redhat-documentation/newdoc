39ef24cf045e005a7f121bc52a535c40fdeeda52
Marek Suchánek <marek.suchanek@protonmail.com>
2020-06-23 18:31:28 +0200

    Make context in assembly IDs optional and conditional

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ struct Options {
 fn main() {
     // Define command-line options
     let cmdline_args = App::new("newdoc")
-        .version("v2.3.2")
+        .version("v2.3.3")
         .author("Marek Suchánek")
         .about("Generate an AsciiDoc file using a modular template")
         // If no arguments are provided, print help
