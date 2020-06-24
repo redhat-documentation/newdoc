14183355b7c6dd9ba52fd042bc418eac1c6f7fdd
Marek Suchánek <marek.suchanek@protonmail.com>
2020-06-24 20:03:46 +0200

    Copy all build artifacts on MS Windows

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -21,7 +21,7 @@ main() {
     cross rustc --bin newdoc --target $TARGET --release -- -C lto
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/newdoc $stage/
+    cp target/$TARGET/release/* $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
