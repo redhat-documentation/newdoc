91201075c10969f80bacf33df0139b468a58ac42
Marek Suchánek <marek.suchanek@protonmail.com>
2020-06-24 19:10:17 +0200

    Run newdoc with --version so that it exits with code 0

--- ci/script.sh
+++ ci/script.sh
@@ -11,8 +11,8 @@ main() {
         return
     fi
 
-    # cross test --target $TARGET
-    # cross test --target $TARGET --release
+    cross test --target $TARGET
+    cross test --target $TARGET --release
 
     cross run --target $TARGET -- --version
     cross run --target $TARGET --release -- --version
