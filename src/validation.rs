4c7663a29fe26d9be242d69fe2e8ad65b1c70fce
Marek Suchánek <marek.suchanek@protonmail.com>
2021-06-02 23:04:42 +0200

    Improve the basic reporting on the module type; issue #11

--- src/validation.rs
+++ src/validation.rs
@@ -9,9 +9,10 @@ use log::{debug, error, info, warn};
 use crate::module::ModuleType;
 
 pub fn validate(file_name: &str) {
-    info!("Validating file `{}`", file_name);
-
+    debug!("Validating file `{}`", file_name);
+    
     let path = Path::new(file_name);
+    let base_name = path.file_name().unwrap().to_str().unwrap();
 
     let read_result = fs::read_to_string(path);
     let content = match read_result {
@@ -22,26 +23,39 @@ pub fn validate(file_name: &str) {
         }
     };
 
-    let base_name = path.file_name().unwrap().to_str().unwrap();
-
     let mod_type = determine_mod_type(base_name, &content);
-    info!("`{}`: Module type is {:?}.", file_name, mod_type.unwrap());
 }
 
-fn determine_mod_type(file_name: &str, content: &str) -> Option<ModuleType> {
-    if file_name.starts_with("assembly_") || file_name.starts_with("assembly-") {
-        return Some(ModuleType::Assembly);
-    }
-    let mod_patterns = [
-        ("con", ":_module-type: CONCEPT", ModuleType::Concept),
-        ("proc", ":_module-type: PROCEDURE", ModuleType::Procedure),
-        ("ref", ":_module-type: REFERENCE", ModuleType::Reference),
-    ];
-    for pattern in mod_patterns.iter() {
-        if file_name.starts_with(pattern.0) || content.contains(pattern.1) {
-            return Some(pattern.2);
+fn determine_mod_type(base_name: &str, content: &str) -> Option<ModuleType> {
+    // An inner function to encapsulate the logic.
+    // This can't be a simple expression block because the logic involves
+    // stuff like for loops.
+    let inner = || {
+        if base_name.starts_with("assembly_") || base_name.starts_with("assembly-") {
+            return Some(ModuleType::Assembly);
         }
+        let mod_patterns = [
+            ("con", ":_module-type: CONCEPT", ModuleType::Concept),
+            ("proc", ":_module-type: PROCEDURE", ModuleType::Procedure),
+            ("ref", ":_module-type: REFERENCE", ModuleType::Reference),
+        ];
+        for pattern in mod_patterns.iter() {
+            if base_name.starts_with(pattern.0) || content.contains(pattern.1) {
+                return Some(pattern.2);
+            }
+        }
+        error!("`{}`: Cannot determine the module type.", base_name);
+        return None;
+    };
+
+    // Run the inner function
+    let mod_type = inner();
+
+    // Report on the value received from the inner function
+    match mod_type {
+        None => { error!("`{}`: Cannot determine the module type.", base_name); },
+        Some(mod_type) => { info!("`{}`: Module type is {}.", base_name, mod_type); }
     }
-    error!("`{}`: Cannot determine the module type.", file_name);
-    return None;
+
+    mod_type
 }
