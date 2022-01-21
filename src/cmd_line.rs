0e9dfc1b4eaed8162c7281d7e63f9a4aa5980e81
Marek Suchánek <marek.suchanek@protonmail.com>
2022-01-21 17:14:18 +0100

    Update deprecated clap interfaces

--- src/cmd_line.rs
+++ src/cmd_line.rs
@@ -1,6 +1,6 @@
 /// This module defines the command-line arguments and behavior of `newdoc`.
 /// It relies on the `clap` crate.
-use clap::{app_from_crate, App, AppSettings, Arg, ArgGroup, ArgMatches};
+use clap::{app_from_crate, AppSettings, Arg, ArgGroup, ArgMatches};
 
 /// Define the command-line arguments and return them as the `clap::ArgMatches` struct.
 pub fn get_args() -> ArgMatches {
@@ -9,96 +9,96 @@ pub fn get_args() -> ArgMatches {
         // If no arguments are provided, print help
         .setting(AppSettings::ArgRequiredElseHelp)
         .arg(
-            Arg::with_name("assembly")
+            Arg::new("assembly")
                 .short('a')
                 .long("assembly")
                 .takes_value(true)
                 .value_name("title")
-                .multiple(true)
+                .multiple_occurrences(true)
                 .help("Create an assembly file"),
         )
         .arg(
-            Arg::with_name("include-in")
+            Arg::new("include-in")
                 .short('i')
                 .long("include-in")
                 .takes_value(true)
                 .value_name("title")
-                .multiple(false)
+                .multiple_occurrences(false)
                 .help("Create an assembly that includes the other specified modules"),
         )
         .arg(
-            Arg::with_name("concept")
+            Arg::new("concept")
                 .short('c')
                 .long("concept")
                 .takes_value(true)
                 .value_name("title")
-                .multiple(true)
+                .multiple_occurrences(true)
                 .help("Create a concept module"),
         )
         .arg(
-            Arg::with_name("procedure")
+            Arg::new("procedure")
                 .short('p')
                 .long("procedure")
                 .takes_value(true)
                 .value_name("title")
-                .multiple(true)
+                .multiple_occurrences(true)
                 .help("Create a procedure module"),
         )
         .arg(
-            Arg::with_name("reference")
+            Arg::new("reference")
                 .short('r')
                 .long("reference")
                 .takes_value(true)
                 .value_name("title")
-                .multiple(true)
+                .multiple_occurrences(true)
                 .help("Create a reference module"),
         )
         .arg(
-            Arg::with_name("validate")
+            Arg::new("validate")
                 .short('l')
                 .long("validate")
                 .takes_value(true)
                 .value_name("file")
-                .multiple(true)
+                .multiple_occurrences(true)
                 .help("Validate (lint) an existing module or assembly file")
         )
         // This group specifies that you either generate modules or validate existing ones
         .group(
-            ArgGroup::with_name("required")
+            ArgGroup::new("required")
                 .args(&["assembly", "concept", "procedure", "reference", "validate"])
                 .required(true)
                 .multiple(true),
         )
         .arg(
-            Arg::with_name("no-comments")
+            Arg::new("no-comments")
                 .short('C')
                 .long("no-comments")
                 .help("Generate the file without any comments"),
         )
         .arg(
-            Arg::with_name("no-examples")
+            Arg::new("no-examples")
                 .short('E')
                 .long("no-examples")
                 .alias("expert-mode")
                 .help("Generate the file without any example, placeholder content"),
         )
         .arg(
-            Arg::with_name("detect-directory")
+            Arg::new("detect-directory")
                 .short('D')
                 .long("detect-directory")
                 .help("Detect the include path, rather than using the <path> placeholder. This is now the default behavior")
                 // I'm enabling this option by default, without a way to disable it.
                 // Let the users test the new behavior and evaluate.
-                .hidden(true),
+                .hide(true),
         )
         .arg(
-            Arg::with_name("no-prefixes")
+            Arg::new("no-prefixes")
                 .short('P')
                 .long("no-prefixes")
                 .help("Do not use module type prefixes (such as `proc_`) in IDs and file names"),
         )
         .arg(
-            Arg::with_name("target-dir")
+            Arg::new("target-dir")
                 .short('T')
                 .long("target-dir")
                 .takes_value(true)
@@ -106,14 +106,14 @@ pub fn get_args() -> ArgMatches {
                 .help("Save the generated files in this directory"),
         )
         .arg(
-            Arg::with_name("verbose")
+            Arg::new("verbose")
                 .short('v')
                 .long("verbose")
                 .help("Display additional, debug messages")
                 .conflicts_with("quiet"),
         )
         .arg(
-            Arg::with_name("quiet")
+            Arg::new("quiet")
                 .short('q')
                 .long("quiet")
                 .help("Hide info-level messages. Display only warnings and errors")
