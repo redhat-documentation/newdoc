2b72aff114af63cbc0de447289ef883e05167df7
Marek Suchánek <msuchane@redhat.com>
2022-07-29 14:00:10 +0200

    Enable some lints and restrictions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+// Enable additional clippy lints by default.
+#![warn(
+    clippy::pedantic,
+    clippy::unwrap_used,
+    clippy::clone_on_ref_ptr,
+    clippy::todo
+)]
+// Disable the documentation clippy lint, so that it stops suggesting backticks around AsciiDoc.
+#![allow(clippy::doc_markdown)]
+// Forbid unsafe code in this program.
+#![forbid(unsafe_code)]
+
 use clap::ArgMatches;
 use color_eyre::eyre::Result;
 
