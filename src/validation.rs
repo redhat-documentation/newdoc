abf989e5457edabbedea331fd5eb206a0e9da098
Marek Suchánek <msuchane@redhat.com>
2022-08-18 19:14:53 +0200

    Add boilerplate notices to all source files

--- src/validation.rs
+++ src/validation.rs
@@ -1,3 +1,21 @@
+/*
+newdoc: Generate pre-populated documentation modules formatted with AsciiDoc.
+Copyright (C) 2022  Marek Suchánek  <msuchane@redhat.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /// This module provides functionality to validate (lint) existing module and assembly files,
 /// to check if the files meet the template structure and other requirements.
 use std::ffi::OsStr;
