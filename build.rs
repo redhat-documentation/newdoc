0cdde0d9b34582df961ab2eb7de2cc81f419a8f3
Marek Suchánek <msuchane@redhat.com>
2023-07-26 16:37:54 +0200

    Update dependencies, especially bpaf

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 //! This script auto-generates a man page from the CLI configuration.
 
-use bpaf::Section;
+use bpaf::doc::Section;
+use chrono::prelude::*;
 
 // We're reusing the module just for the Cli struct. Ignore the rest of the code
 // and don't report it as "never used" in this build script.
@@ -11,24 +12,22 @@ mod cmd_line;
 // Man page metadata
 const CARGO_PKG_NAME: &str = env!("CARGO_PKG_NAME");
 const SECTION: Section = Section::General;
-const DATE: &str = "February 2023";
 const CARGO_PKG_AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
-const CARGO_PKG_REPOSITORY: &str = env!("CARGO_PKG_REPOSITORY");
-const CARGO_PKG_HOMEPAGE: &str = env!("CARGO_PKG_HOMEPAGE");
 
 fn main() -> std::io::Result<()> {
     let out_dir =
         std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
 
+    let date = current_date();
+
     let parser = cmd_line::cli();
 
-    let man_page = parser.as_manpage(
+    let man_page = parser.render_manpage(
         CARGO_PKG_NAME,
         SECTION,
-        DATE,
-        CARGO_PKG_AUTHORS,
-        CARGO_PKG_HOMEPAGE,
-        CARGO_PKG_REPOSITORY,
+        Some(&date),
+        Some(CARGO_PKG_AUTHORS),
+        None,
     );
 
     let man_name = format!("{CARGO_PKG_NAME}.1");
@@ -37,4 +36,16 @@ fn main() -> std::io::Result<()> {
     std::fs::write(man_path, man_page)?;
 
     Ok(())
+}
+
+/// Generate the current date to mark the last update of the man page.
+/// The format is "Month Year".
+fn current_date() -> String {
+    let utc: DateTime<Utc> = Utc::now();
+    let year = utc.year();
+    let month = Month::try_from(utc.month() as u8)
+        .expect("The current month is out of range.")
+        .name();
+
+    format!("{month} {year}")
 }
